4e427821c280e0d3bf56a81183ba0efe3bd54571
Nurdana Kozhamuratu <n/kozhamurat@mail.ru>
2026-02-04 11:48:01 +0500

    .

---
+++ Practice3/functions/basic_functions.py
@@ -0,0 +1,27 @@
+#1
+def my_function():
+  print("Hello from a function")
+
+my_function()  
+
+
+#2
+def my_function():
+  print("Hello from a function")
+
+my_function()
+my_function()
+my_function()
+
+
+
+#3
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * 5 / 9
+
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
+
+#4
