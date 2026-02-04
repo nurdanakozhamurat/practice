67f4f9d3fcf359d955a285bf762769bf3a9784ea
Nurdana Kozhamuratu <n/kozhamurat@mail.ru>
2026-02-05 00:54:46 +0500

    .

--- Practice3/functions/function_arguments.py
+++ Practice3/functions/function_arguments.py
@@ -38,3 +38,23 @@ def my_function(vechicle, name):
 
 my_function( "car", "Lexus" )
 
+
+#6
+def my_function(fruits):
+  for fruit in fruits:
+    print(fruit)
+
+my_fruits = ["apple", "banana", "cherry"]
+my_function(my_fruits)
+
+
+#7
+def my_function(person):
+  print("Name:", person["name"])
+  print("Age:", person["age"])
+
+my_person = {"name": "Emil", "age": 25}
+my_function(my_person)
+
+
+
