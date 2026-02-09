541615437aa3d485a4a81a0f815ff9cf8b7c35a8
Nurdana Kozhamuratu <n/kozhamurat@mail.ru>
2026-02-09 23:08:22 +0500

    ,

---
+++ Practice3/inheritance/inheritance_basics.py
@@ -0,0 +1,28 @@
+#Parent class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+x = Person("Nurdana", "Kozhamurat")
+x.printname()
+
+
+#Child class
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+class Student(Person):
+  pass
+
+x = Student("Madina", "Ibragim")
+x.printname()
+
